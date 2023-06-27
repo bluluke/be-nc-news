fd0e75ebfcfd11b3906e2c8c35f38b5845aee45c
bluluke <bluluke@hotmail.com>
2023-06-27 12:16:52 +0100

    Test Pass/Add Endpoint: GET /API return list of objects representing endpoints available.

--- db/app.js
+++ db/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 
 
 const { getTopics } = require('./controllers/topicsController');
+const { getEndpoints } = require('./controllers/endpointsController');
 
 app.get("/api/topics/", getTopics);
+
+// app.get("/api", (req, res) => {
+//     res.status(200).send(endpointsData);
+// });
+
+app.get("/api", getEndpoints);
+
 
 module.exports = app; 
