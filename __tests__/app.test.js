d1e8299c85ab1be7e9a41bd5c7f5d7c9df635241
bluluke <bluluke@hotmail.com>
2023-06-28 08:50:30 +0100

    Refactor Test: GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,41 +56,17 @@ describe('GET /api/articles/:article_id', () => {
       const article = articleData.body[0];
       expect(article).toHaveProperty("author", expect.any(String));
       expect(article).toHaveProperty("title", expect.any(String));
-      expect(article).toHaveProperty("article_id", expect.any(Number));
+      expect(article).toHaveProperty("article_id", 1);
       expect(article).toHaveProperty("body", expect.any(String));
       expect(article).toHaveProperty("topic", expect.any(String));
       expect(article).toHaveProperty("created_at", expect.any(String));
       expect(article).toHaveProperty("votes", expect.any(Number));
       expect(article).toHaveProperty("article_img_url", expect.any(String));
     })
-  })
-  test('200: responds with articleobject requested', () => {
-    return request(app)
-    .get("/api/articles/3")
-    .expect(200)
-    .then((articleData) => {
-      const article = articleData.body[0];
-      expect(article).toHaveProperty("author", expect.any(String));
-      expect(article).toHaveProperty("title", expect.any(String));
-      expect(article).toHaveProperty("article_id", expect.any(Number));
-      expect(article).toHaveProperty("body", expect.any(String));
-      expect(article).toHaveProperty("topic", expect.any(String));
-      expect(article).toHaveProperty("created_at", expect.any(String));
-      expect(article).toHaveProperty("votes", expect.any(Number));
-      expect(article).toHaveProperty("article_img_url", expect.any(String));
-    })
-  })
+  });
   test('404: ERROR returns message if id does not exist', () => { 
     return request(app)
     .get("/api/articles/100")
-    .expect(404)
-    .then(({ body }) => {
-      expect(body.msg).toBe('Not found');
-    })
-  });
-  test('404: ERROR responds with message if id does not exist', () => { 
-    return request(app)
-    .get("/api/articles/50")
     .expect(404)
     .then(({ body }) => {
       expect(body.msg).toBe('Not found');
